3297d93231d94a5706d0bdec2385855fdf277195
Ryan Schmelter <rschmelter33@gmail.com>
2017-11-17 01:02:32 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
 
+class CashRegister
+
+  attr_accessor :total, :discount
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(item, price, quantity = 0)
+    if quantity > 0
+      @total += price * quantity
+    else
+      @total += price
+    end
+  end
+    def apply_discount
+      discount_percent = @discount / 100
+      discount_in_dollars = @total * discount_percent
+      @total = @total - discount_in_dollars
+      
+    end
+
+
+
+
+
+
+
+end
